ca31713373f21d53c4f4305f3f192fd8b12c1ec4
dayeon123 <choidayeon1230@gmail.com>
2025-03-14 18:07:12 +0900

    250314

---
+++ 250314/project01/src/components/TodoEditor.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useRef } from "react";
+
+const TodoEditor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const inputRef = useRef(3);
+
+  const onChangeContent = (e) => {
+    e.preventDefault();
+    setContent(e.target.value);
+  };
+  const onSubmit = () => {
+    if (!content) {
+      inputRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div className="TodoEditor">
+      <h4>새로운 Todo 작성하기 ✍🏻</h4>
+      <form className="editor_wrapper" name="todolist" action="#" method="get">
+        <input
+          type="text"
+          ref={inputRef}
+          value={content}
+          onChange={onChangeContent}
+          onKeyDown={onKeyDown}
+          placeholder="새로운 Todo..."
+        />
+        <input type="button" value="추가" onClick={onSubmit} />
+      </form>
+    </div>
+  );
+};
+
+export default TodoEditor;
