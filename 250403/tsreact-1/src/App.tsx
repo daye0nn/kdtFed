eb6307c6afe5f0c2fb6aea152c4263ae33d14498
dayeon123 <choidayeon1230@gmail.com>
2025-04-03 16:41:34 +0900

    250403

---
+++ 250403/tsreact-1/src/App.tsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import GlobalStyles from "./styles/GlobalStyles.styles";
+import styled from "styled-components";
+import Header from "./components/Header";
+import BlogPost from "./components/BlogPost";
+import Button from "./components/Button";
+import Form from "./components/Form";
+// import mockPosts from "./mock/posts.json";
+
+const Container = styled.div`
+  width: 100%;
+  /* height: 100vh; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: var(--bg-color);
+  overflow: scroll;
+`;
+
+const ButtonContainer = styled.div`
+  position: absolute;
+  right: 40px;
+  bottom: 40px;
+`;
+
+interface Post {
+  readonly userId: number;
+  readonly id: number;
+  readonly title: string;
+  readonly body: string;
+}
+
+function App() {
+  const [posts, setPosts] = useState<Array<Post>>([]);
+
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  useEffect(() => {
+    // setTimeout(() => {
+    //   setPosts(mockPosts);
+    // }, 1000);
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setPosts(json))
+      .catch((error) => console.error(error));
+  }, []);
+  return (
+    <>
+      <GlobalStyles />
+      <Container>
+        <Header />
+        {posts.map((post) => (
+          <BlogPost key={post.id} title={post.title} body={post.body} />
+        ))}
+        <ButtonContainer>
+          <Button label="등록" />
+        </ButtonContainer>
+        <Form />
+      </Container>
+    </>
+  );
+}
+
+export default App;
